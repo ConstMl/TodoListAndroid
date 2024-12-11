f6e103a0288730573c3f2f800e0a1d634db2ee67
Konst <malinov.konstantin@joy-dev.com>
2024-12-12 02:58:30 +0300

    add remove task confirm dialog

--- app/src/main/java/com/example/todolisttest/presentation/screens/todo_list_screen/TodoListScreen.kt
+++ app/src/main/java/com/example/todolisttest/presentation/screens/todo_list_screen/TodoListScreen.kt
@@ -4,23 +4,50 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.todolisttest.R
+import com.example.todolisttest.domain.model.TaskListItemModel
 import com.example.todolisttest.presentation.theme.OverlappingHeight
+import com.example.todolisttest.presentation.widget.CommonDialog
 import com.example.todolisttest.presentation.widget.CustomCircularProgressIndicator
 
 @Composable
 internal fun TodoListScreen() {
     val viewModel = hiltViewModel<TodoListViewModel>()
     val state by viewModel.state.collectAsState()
+    var openRemoveDialog by remember { mutableStateOf(false) }
+    var taskForRemove by remember { mutableStateOf<TaskListItemModel?>(null) }
+
     if (state.isLoading) {
         CustomCircularProgressIndicator(modifier = Modifier.fillMaxSize())
     } else {
         TodoItemsContainer(
             todoItems = state.taskList,
             onItemClick = viewModel::onItemClick,
-            onItemDelete = viewModel::onItemRemove,
+            onItemDelete = { item ->
+                taskForRemove = item
+                openRemoveDialog = true
+            },
             overlappingElementsHeight = OverlappingHeight
+        )
+    }
+    if (openRemoveDialog) {
+        CommonDialog(
+            title = stringResource(
+                id = R.string.confirm_remove_task_dialog_title
+            ),
+            onConfirm = {
+                taskForRemove?.let(viewModel::onItemRemove)
+                openRemoveDialog = false
+            },
+            onCancel = {
+                openRemoveDialog = false
+            }
         )
     }
 }
--- app/src/main/java/com/example/todolisttest/presentation/widget/CommonDialog.kt
+++ app/src/main/java/com/example/todolisttest/presentation/widget/CommonDialog.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -57,6 +58,7 @@ internal fun CommonDialog(
                 style = DialogTitleTextStyle.copy(
                     color = MaterialTheme.colorScheme.tertiary
                 ),
+                textAlign = TextAlign.Center
             )
             Row(
                 modifier = Modifier
@@ -100,6 +102,6 @@ internal fun CommonDialog(
 @Preview
 private fun CommonDialogComposePreview() {
     CommonDialog(
-        title = stringResource(id = R.string.app_bar_name)
+        title = stringResource(id = R.string.confirm_remove_task_dialog_title)
     )
 }
