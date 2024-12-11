183a94bbfd1ffcf6008f96f20404392247991be8
Konst <malinov.konstantin@joy-dev.com>
2024-12-11 16:14:35 +0300

    ref: remove api comment

--- app/src/main/java/com/example/todolisttest/domain/model/TaskListItemModel.kt
+++ app/src/main/java/com/example/todolisttest/domain/model/TaskListItemModel.kt
@@ -15,35 +15,3 @@ data class TaskListItemModel(
         )
     }
 }
-
-/**
- * todo: API example
- * https://dummyjson.com/todos?limit=3&skip=0
- * skip - page
- * limit - pageSize
- * {
- *     "todos": [
- *         {
- *             "id": 1,
- *             "todo": "Do something nice for someone you care about",
- *             "completed": false,
- *             "userId": 152
- *         },
- *         {
- *             "id": 2,
- *             "todo": "Memorize a poem",
- *             "completed": true,
- *             "userId": 13
- *         },
- *         {
- *             "id": 3,
- *             "todo": "Watch a classic movie",
- *             "completed": true,
- *             "userId": 68
- *         }
- *     ],
- *     "total": 254,
- *     "skip": 0,
- *     "limit": 3
- * }
- * */
